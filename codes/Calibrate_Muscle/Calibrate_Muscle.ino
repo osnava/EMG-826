40c6b4c9c0ce2546044a42380208f84f1b2ac2c3
osnava <66899625+osnava@users.noreply.github.com>
2020-08-18 19:11:13 -0500

    Update Calibrate_Muscle.ino

--- codes/Calibrate_Muscle/Calibrate_Muscle.ino
+++ codes/Calibrate_Muscle/Calibrate_Muscle.ino
@@ -1,6 +1,6 @@
 /*Use this code to calibrate your sensor. Know what's your minimum and maximum values.
   
-  @author   Oscar Navarrete
+  @author   Oscar Navarrete 💪
 
 -Pinout Diagrams:
 Arduino UNO   https://bit.ly/30Axk7l
